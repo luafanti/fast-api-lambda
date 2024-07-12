0e7aad9fc5e48616cedb58a16681e76144f08e34
Artur Bartosik <luafanti@gmail.com>
2024-07-12 13:54:20 +0200

    Add Secret handling from Lambda secret extension

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import os
+import urllib3
+import json
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from mangum import Mangum
+
 
 app = FastAPI()
 
@@ -8,6 +12,25 @@ app = FastAPI()
 class Message(BaseModel):
     message: str
 
+
+def get_aws_secret():
+    http = urllib3.PoolManager()
+    headers = {"X-Aws-Parameters-Secrets-Token": os.environ.get('AWS_SESSION_TOKEN')}
+    secrets_extension_http_port = "2773"
+    secrets_extension_endpoint = "http://localhost:" + \
+                                 secrets_extension_http_port + \
+                                 "/secretsmanager/get?secretId=" + \
+                                 "MongoDBSecret-dev"
+
+    resp = http.request(
+        "GET",
+        secrets_extension_endpoint,
+        headers=headers
+    )
+    print('Secret data:', resp.data)
+    secret = json.loads(resp.data)
+    secretValue = json.loads(secret['SecretString'])
+    return secretValue
 
 @app.post("/asset-management/v1/metadata-generation")
 async def metadata_generation(body: Message):
@@ -19,7 +42,8 @@ async def metadata_generation(body: Message):
 
 @app.get("/asset-management/v1/metadata/feedback")
 async def get_feedback():
-    return {"message": "Feedback retrieved successfully"}
+    mongo_db_secret = get_aws_secret()
+    return {"message": "Feedback retrieved successfully", "secret": mongo_db_secret}
 
 
 @app.post("/asset-management/v1/metadata-generation")
